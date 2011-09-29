9a0bf1d3844c075e416c73f124b66875344f285c
hellojinjie <hellojinjie@163.com>
2011-09-29 19:03:01 +0800

    修改构建脚本

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# build file for polaris
+# build script for polaris
 
 #
 # Polaris 构建脚本
-HELP_TEXT=" \n 1. 不带任何参数: 显示本文档\n \
+HELP_TEXT=" \n 1. 不带任何参数,或参数 help: 显示本帮助文档\n \
     2. 参数 all: 构建整个系统(borealis, marshal, head)\n \
-    3. 参数 init: 仅初始化环境，不进行任何编译 \n \
-    4. 参数 help: 显示本文档 \n"
+    3. 参数 init: 仅初始化环境，不进行任何编译 \n  "
 #
 
 export CVS_SANDBOX=$HOME/Workspace/borealis_summer_2008
@@ -21,22 +20,27 @@ if [ "x$TOOL_PATH_SET" = "x" ]; then
     export PATH=/home/jj/Workspace/borealis_summer_2008/polaris/tool/head:$PATH
 fi
 
-case "x$1" in
-    "x" )
-        echo -e $HELP_TEXT
-        ;;
-    "xinit" )
-        echo "Environment is set... Ready to work..."
-        ;;
-    "xhelp" )
-        echo -e $HELP_TEXT
-        ;;
-    "xall" )
-        cd utility/unix/
-        ./build.borealis.sh
-        ./build.borealis.sh -client -tool.marshal -tool.head
-        echo "Exit directory: " $(pwd)
-        cd -
-        ;;
-esac
+# 判断这个脚本是在独立的子进程中执行的，还是在当前进程通过 source 运行的.
+if [ "x$0" = "xbash" ]; then
+    echo "Environment is initialized... Ready to work..."
+else
+    case "x$1" in
+        "x" | "xhelp" )
+            echo -e $HELP_TEXT
+            ;;
+        "xinit" )
+            echo "Use source to initialize environment...try  source build.sh"
+            ;;
+        "xall" )
+            cd utility/unix/
+            ./build.borealis.sh
+            ./build.borealis.sh -client -tool.marshal -tool.head
+            echo "Exit directory: " $(pwd)
+            cd -
+            ;;
+        * )
+            echo "Unknow option: "$1
+            ;;
+    esac
+fi
 
