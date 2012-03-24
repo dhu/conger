050bd72864c3f4d3e065ccf11bf1ca780c05b0eb
hellojinjie <hellojinjie@163.com>
2012-03-24 15:19:32 +0800

    删除旧版的borealis demo

--- demo/ha/cleaner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-while read line  # For as many lines as the input file has...
-do
-line=${line/paste:/}   # Output the line itself.
-line=${line/capture:/}
-echo $line
-done
-
-exit 0
--- demo/ha/sieve.m
+++
@@ -1,9 +0,0 @@
-function [c, l] = sieve(input, g)
-    c = input(find(input(:,2) == g), [1 6 7 8]);
-    l = mean(input(find(input(:,2) == g), 5));
-    times = c(:, 1);
-    times = times - [times(1); times(1:end-1)];
-    c(:, 1) = times*0.001;
-    c = c(2:end, :);
-    c = sortrows(c, 1);
-end
